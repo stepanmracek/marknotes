fddfb552b7a358c729a6a914d4bbcbeca20a8bc0
Štěpán Mráček <stepan.mracek@gmail.com>
2014-02-21 11:27:20 +0100

    en and cs localization files

---
+++ src/marknotes_cs.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">MarkNotes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="246"/>
+        <source>Add new note</source>
+        <translation type="unfinished">Přidat novou poznámku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="192"/>
+        <location filename="mainwindow.cpp" line="246"/>
+        <location filename="mainwindow.cpp" line="290"/>
+        <source>Enter new note name</source>
+        <translation type="unfinished">Zadejte název nové poznámky</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="215"/>
+        <location filename="mainwindow.cpp" line="266"/>
+        <location filename="mainwindow.cpp" line="304"/>
+        <location filename="mainwindow.cpp" line="325"/>
+        <source>Information</source>
+        <translation type="unfinished">Informace</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="215"/>
+        <location filename="mainwindow.cpp" line="304"/>
+        <source>Note already exists</source>
+        <translation type="unfinished">Poznámka již existuje</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="266"/>
+        <location filename="mainwindow.cpp" line="325"/>
+        <source>File or folder already exists</source>
+        <translation type="unfinished">Složka se zadaným názvem již existuje </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="290"/>
+        <source>Rename note</source>
+        <translation type="unfinished">Přejmenovat poznámku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="314"/>
+        <source>Rename Folder</source>
+        <translation type="unfinished">Přejmenovat složku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="314"/>
+        <source>Enter new folder name</source>
+        <translation type="unfinished">Zadejte nový název složky</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="379"/>
+        <source>Question</source>
+        <translation type="unfinished">Otázka</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="380"/>
+        <source>Delete current note?</source>
+        <translation type="unfinished">Smazat aktuální poznámku?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="381"/>
+        <source>Delete current folder and all its content?</source>
+        <translation type="unfinished">Smazat vybranou složku a celý její obsah?</translation>
+    </message>
+</context>
+</TS>
